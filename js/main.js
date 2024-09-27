6eb1efdaaf2b8b05693685f7a2c63404a65f3574
DESKTOP-RIMIB5Q\jpribio <jaime.p.r023@gmail.com>
2024-09-27 09:14:32 +0200

    Terminada

--- js/main.js
+++ js/main.js
@@ -1,32 +1,30 @@
-import { div, mult, resta, suma } from "./operador";
+import { div, mult, resta, suma } from "./operador.js";
 
-function calcularResultado(){
-    let numero1 = document.querySelector("#numero1").value;
+function calcularResultado() {
+    let numero1 = parseFloat(document.querySelector("#numero1").value);
     let operador = document.querySelector("#operador").value;
-    let numero2 = document.querySelector("#numero2").value;
+    let numero2 = parseFloat(document.querySelector("#numero2").value);
     let resultado;
     console.log(numero1);
     console.log(operador);
     console.log(numero2);
-    if(numero1!==null && numero2!==null){
-        if(operador==="suma"){
-            resultado=suma(numero1,numero2);
-            document.querySelector("#resultado").innerHTML=resultado;
-        }else if(operador==="resta"){
-            resultado = resta(numero1,numero2);
-            document.querySelector("#resultado").innerHTML=resultado;
-        }else if(operador==="mult"){
-            resultado = mult(numero1,numero2);
-            document.querySelector("#resultado").innerHTML=resultado;
-        }else if(operador==="div"){
-            resultado = div(numero1,numero2);
-            document.querySelector("#resultado").innerHTML=resultado;
-        }else{
-             alert("No has elegido ningun operador");  
+    if (numero1 !== null && numero2 !== null) {
+        if (operador === "suma") {
+            resultado = suma(numero1, numero2);
+        } else if (operador === "resta") {
+            resultado = resta(numero1, numero2);
+        } else if (operador === "mult") {
+            resultado = mult(numero1, numero2);
+        } else if (operador === "div") {
+            resultado = div(numero1, numero2);
+        } else {
+            alert("No has elegido ningun operador");
         }
-        
-    }else{
+        document.querySelector("#resultado").innerHTML = `Resultado : ${resultado}`;
+
+    } else {
         alert("Algun numero es nulo, por favor elige algun numero");
     }
-    
 }
+
+window.calcularResultado=calcularResultado;
--- js/operador.js
+++ js/operador.js
@@ -15,6 +15,10 @@ export function mult(n1,n2){
 }
 
 export function div(n1,n2){
+ if (n2===0) {
+    return 'Error : Division por 0';
+ } else {
     let resultado = n1/n2;
     return resultado;
+ }
 }
